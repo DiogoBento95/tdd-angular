9b267c701ded3060063f6155fe0581937a5120ab
Diogo Bento <diogo.bento@bringglobal.com>
2023-05-10 16:13:31 +0000

    feat: TDD Home list.

--- src/app/src/component/homes/homes.component.ts
+++ src/app/src/component/homes/homes.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-homes',
   templateUrl: './homes.component.html',
   styleUrls: ['./homes.component.less']
 })
-export class HomesComponent {
+export class HomesComponent implements OnInit{
+
+  homes$: any;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+
+    this.homes$ = this.dataService.getHomes$();
+
+  }
+
 
 }
